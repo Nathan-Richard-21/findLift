97d5be81b128a2e2215d8d31b4460edbd61c7292
Nathan-Richard-21 <117543851+Nathan-Richard-21@users.noreply.github.com>
2025-12-05 17:12:38 +0200

    fix try

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tantml:query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ToastProvider } from './contexts/ToastContext'
 import App from './App.jsx'
 import './index.css'
